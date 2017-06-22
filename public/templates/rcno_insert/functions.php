245fa224688d179bffd613e3bc1357f99bca73f8
w33zy <wzy@outlook.com>
2017-06-22 08:29:39 -0500

    created the 'Insert' template

---
+++ public/templates/rcno_insert/functions.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Registers the stylesheet for the 'Basic' template.
+ *
+ * @uses wp_enqueue_style
+ * @return void
+ */
+
+add_action( 'wp_enqueue_scripts', 'rcno_insert_styles' );
+add_action( 'wp_enqueue_scripts', 'rcno_insert_scripts' );
+
+function rcno_insert_styles() {
+	wp_enqueue_style( 'rcno-insert-style',  plugin_dir_url( __FILE__ ) . '/insert-style.css', array(), '1.0.0', 'all' );
+}
+
+function rcno_insert_scripts() {
+	wp_enqueue_script( 'rcno-insert-script',  plugin_dir_url( __FILE__ ) . '/insert-script.js', array( 'jquery' ), '1.0.0', true );
+}
+
+
+function rcno_insert_book_details( $content ) {
+	// Get the review ID.
+	if ( isset( $GLOBALS['review_id'] ) && $GLOBALS['review_id'] !== '' ) {
+		$review_id = $GLOBALS['review_id'];
+	} else {
+		$review_id = get_the_ID();
+	}
+
+	$template = new Rcno_Template_Tags( 'rcno-reviews', '1.0.0' );
+
+	if ( is_single() && is_singular( 'rcno_review' ) ) {
+		$content_block = explode( '<p>', $content );
+		$count = count( $content_block );
+
+		if ( ! empty( $content_block[1] ) ) {
+			$content_block[1] .= $template->get_the_rcno_full_book_details( $review_id, 'medium' );
+		}
+		for ( $i = 1; $i < $count; $i++ ) {
+			$content_block[ $i ] = '<p>' . $content_block[ $i ];
+		}
+		$content = implode( '', $content_block );
+	}
+
+	return $content;
+}
+
+add_filter( 'the_content', 'rcno_insert_book_details' );
---
+++ public/templates/rcno_insert/review.php
@@ -0,0 +1,69 @@
+<?php
+/*
+Author: wzyMedia
+Author Mail: kemory@wzymedia.com
+Author URL: https://wzymedia.com
+Layout Name: Rcno Insert
+Version: 1.0.0
+Description: The 'insert' book review layout.
+*/
+
+
+// Get the review ID.
+if ( isset( $GLOBALS['review_id'] ) && $GLOBALS['review_id'] !== '' ) {
+	$review_id = $GLOBALS['review_id'];
+} else {
+	$review_id = get_post()->ID;
+}
+
+$plugin_name = 'rcno-reviews';
+$version = '1.0.0';
+
+$template = new Rcno_Template_Tags( $plugin_name, $version );
+$review_score_enable = (bool) get_post_meta( $review_id, 'rcno_review_score_enable', true );
+$review_score_position = get_post_meta( $review_id, 'rcno_review_score_position', true );
+
+//$ratings = new Rcno_Reviews_Public_Rating( $plugin_name, $version );
+
+?>
+
+<?php if ( $template->is_review_embedded() ) : ?>
+    <h2 class="rcno-review-title">
+		<?php echo get_the_title( $review_id ) ?>
+    </h2>
+<?php endif; ?>
+
+<div class="rcno-book-info">
+    <div class="review-content">
+
+	    <?php if ( true === $review_score_enable && 'top' === $review_score_position ) : ?>
+
+            <div class="review-box-container">
+			    <?php $template->rcno_print_review_box( $review_id ); ?>
+            </div>
+
+	    <?php endif; ?>
+
+        <?php
+            // Prints the book review content.
+            $template->the_rcno_book_review_content( $review_id );
+	    ?>
+
+        <?php if ( true === $review_score_enable && 'bottom' === $review_score_position ) : ?>
+
+            <div class="review-box-container">
+                <?php $template->rcno_print_review_box( $review_id ); ?>
+            </div>
+
+        <?php endif; ?>
+
+    </div>
+
+	<?php
+	    // Prints the review and book's metadata in the JSON+LD format content.
+        $template->the_rcno_book_schema_data( $review_id );
+        $template->the_rcno_review_schema_data( $review_id );
+        echo '<!--- Recencio Book Reviews --->';
+	?>
+
+</div>
